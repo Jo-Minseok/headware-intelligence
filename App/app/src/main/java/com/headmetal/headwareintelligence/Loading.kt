ba62568036fc876d41134b880097eb329e975fb7
right5625 <right5625@naver.com>
2024-03-30 17:14:37 +0900

    Loding UI

---
+++ App/app/src/main/java/com/headmetal/headwareintelligence/Loading.kt
@@ -0,0 +1,44 @@
+package com.headmetal.headwareintelligence
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.headmetal.headwareintelligence.ui.theme.HeadwareIntelligenceTheme
+
+@Composable
+fun Loading() {
+    Surface(
+        color = Color(0xFFF9C94C
+        )
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.helmet),
+                contentDescription = null
+            )
+            Text(
+                text = "HeadWear - Intelligence",
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingPreview() {
+    HeadwareIntelligenceTheme {
+        Loading()
+    }
+}
--- App/app/src/main/java/com/headmetal/headwareintelligence/MainActivity.kt
+++ App/app/src/main/java/com/headmetal/headwareintelligence/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.headmetal.headwareintelligence.ui.theme.HeadwareIntelligenceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +13,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HeadwareIntelligenceTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    Loading()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HeadwareIntelligenceTheme {
-        Greeting("Android")
     }
 }

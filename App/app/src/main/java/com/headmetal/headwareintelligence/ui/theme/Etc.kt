880b7642be6aacec3eb912bccc6b687e7313f505
채승룡 <tmdfyd986@gmail.com>
2024-03-31 00:06:42 +0900

    Etc-ing

---
+++ App/app/src/main/java/com/headmetal/headwareintelligence/ui/theme/Etc.kt
@@ -0,0 +1,42 @@
+package com.headmetal.headwareintelligence.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+
+@Preview(showBackground = true)
+@Composable
+fun Etc(){
+    Surface() {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "dddd")
+            Text(text = "AAAA")
+        }
+
+    }
+}

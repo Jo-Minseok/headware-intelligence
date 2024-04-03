703a7329076836202a2243d6af98f4cfa0cce7c5
J_Coder <seok3765@naver.com>
2024-04-04 01:59:49 +0900

    App create

--- Backend/fastapi_back_config.py
+++ Backend/fastapi_back_config.py
@@ -1,4 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Union
+from fastapi import FastAPI
+
+# 기상청 API load
 
 
 class Settings(BaseSettings):
@@ -10,3 +14,16 @@ class Settings(BaseSettings):
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
 print(settings.API_KEY_DECODE)
+
+# FastAPI 백엔드 서버 단
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}

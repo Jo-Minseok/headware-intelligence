704996cb3e411952e43433503373b114fd543a93
right5625 <right5625@naver.com>
2024-04-04 10:42:09 +0900

    API

--- Backend/fastapi_back_config.py
+++ Backend/fastapi_back_config.py
@@ -1,29 +1,23 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import Union
 from fastapi import FastAPI
+import requests
 
 # 기상청 API load
-
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env', env_file_encoding='utf-8')
     API_KEY_ENCODE: str
     API_KEY_DECODE: str
 
-
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
-print(settings.API_KEY_DECODE)
 
 # FastAPI 백엔드 서버 단
 app = FastAPI()
 
+api_endpoint = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0'
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/weather/{city}")
+async def get_weather(city: str):
+    response = requests.get(f"{api_endpoint}/weather?q={city}&appid=" + settings.API_KEY_ENCODE)
+    weather_data = response.json()
+    return weather_data

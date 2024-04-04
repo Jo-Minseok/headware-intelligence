1975aaa96d69c4888e7c2d87190b7ff8d11c84db
right5625 <right5625@naver.com>
2024-04-04 17:47:19 +0900

    Server Setting End

--- Backend/api_server.py
+++ Backend/api_server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import requests, uvicorn, api_config, openpyxl, pytz
 from datetime import datetime
 
@@ -14,8 +14,7 @@ app = FastAPI()
 @app.get("/weather/{city}/{district}")
 async def get_weather(city: str, district: str):
     now = datetime.now(pytz.timezone('Asia/Seoul'))
-    h = now.hour
-    m = now.minute
+    h, m = now.hour, now.minute
     base_time = ('%02d' % h) + '00' if m > 40 else ('%02d' % (h - 1 if h > 0 else 23)) + '00'
     response = requests.get(api_config.api.api_endpoint + '/getUltraSrtNcst'
                             '?serviceKey=' + api_config.api.api_key + 
@@ -25,6 +24,31 @@ async def get_weather(city: str, district: str):
                             '&nx=' + data_dict[city + ' ' + district][0] + 
                             '&ny=' + data_dict[city + ' ' + district][1])
     return response.json()
+    # # 세부 결과 확인
+    # weather_data = response.json()['response']['body']['items']['item']
+    # result = ('위치 : ' + city + ' ' + district + '\n' + 
+    #           '날짜 : ' + weather_data[0]['baseDate'] + '\n' + 
+    #           '기준 시간 : ' + weather_data[0]['baseTime'][:2] + '시\n')
+    # for i in weather_data:
+    #     if i['category'] == 'T1H':
+    #         result += '기온 : ' + i['obsrValue'] + ' °C\n'
+    #     elif i['category'] == 'RN1':
+    #         result += '1시간 강수량 : ' + i['obsrValue'] + ' mm\n'
+    #     elif i['category'] == 'UUU':
+    #         result += '동서 바람 성분 : ' + i['obsrValue'] + ' m/s\n'
+    #     elif i['category'] == 'VVV':
+    #         result += '남북 바람 성분 : ' + i['obsrValue'] + ' m/s\n'
+    #     elif i['category'] == 'REH':
+    #         result += '습도 : ' + i['obsrValue'] + ' %\n'
+    #     elif i['category'] == 'PTY':
+    #         result += '강수 형태 : ' + i['obsrValue'] + '\n'
+    #     elif i['category'] == 'VEC':
+    #         result += '풍향 : ' + i['obsrValue'] + ' deg\n'
+    #     elif i['category'] == 'WSD':
+    #         result += '풍속 : ' + i['obsrValue'] + ' m/s\n'   
+    # result = Response(result)
+    # result.headers['Content-Type'] = 'text/plain; charset=utf-8'
+    # return result
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)

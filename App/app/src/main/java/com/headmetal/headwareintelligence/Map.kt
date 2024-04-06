78a41c6aa0a49fefd5032cd222f0e9327ebecc49
J_Coder <seok3765@naver.com>
2024-04-07 04:57:44 +0900

    [APP] create: Map.kt Mockup

---
+++ App/app/src/main/java/com/headmetal/headwareintelligence/Map.kt
@@ -0,0 +1,17 @@
+package com.headmetal.headwareintelligence
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground= true)
+@Composable
+fun Map(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFF9C94C)
+    ){}
+}

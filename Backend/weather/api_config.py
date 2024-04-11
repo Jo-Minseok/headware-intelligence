6637b6c24a1a9a4366799310dfb9503a576692a8
J_Coder <seok3765@naver.com>
2024-04-11 15:34:40 +0900

    [BE] chore: 파일 위치 수정

--- Backend/weather/api_config.py
+++ Backend/weather/api_config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=r'C:\Users\MINSEOK\Desktop\Git Kraken\headware-intelligence\Backend\weather\.env', env_file_encoding='utf-8')
+        env_file=r'./weather/.env', env_file_encoding='utf-8')
     api_endpoint: str
     api_key: str
 
 
-api = Settings(_env_file=r'C:\Users\MINSEOK\Desktop\Git Kraken\headware-intelligence\Backend\weather\.env', _env_file_encoding='utf-8')
+api = Settings(_env_file=r'./weather/.env', _env_file_encoding='utf-8')

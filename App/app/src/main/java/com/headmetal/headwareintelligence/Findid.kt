06b858489729f02518bf89fcd930aea107090fdc
채승룡 <tmdfyd986@gmail.com>
2024-04-15 16:56:17 +0900

    [APP] feat: 계정 찾기 MockUP

---
+++ App/app/src/main/java/com/headmetal/headwareintelligence/Findid.kt
@@ -0,0 +1,214 @@
+package com.headmetal.headwareintelligence
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Preview(showBackground = true)
+@Composable
+fun Findid(modifier: Modifier = Modifier) {
+    var id by remember {
+        mutableStateOf("")
+    }
+    var pw by remember {
+        mutableStateOf("")
+    }
+    var part by remember {
+        mutableStateOf("None")
+    }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFF9C94C)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.helmet),
+                contentDescription = null
+            )
+            Column(
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Text(
+                    text = "이름",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                TextField(
+                    value = id,
+                    onValueChange = { id = it },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.alpha(0.6f).width(350.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+            Column(
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Text(
+                    text = "생년월일",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                TextField(
+                    value = pw,
+                    onValueChange = { pw = it },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.alpha(0.6f).width(350.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+
+            Column(
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Text(
+                    text = "전화번호",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                TextField(
+                    value = pw,
+                    onValueChange = { pw = it },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.alpha(0.6f).width(350.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+
+            Column(
+                modifier = Modifier.padding(bottom = 30.dp)
+            ) {
+                Text(
+                    text = "이메일",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                TextField(
+                    value = pw,
+                    onValueChange = { pw = it },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier.alpha(0.6f).width(350.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+            }
+
+            Column(
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Text(
+                    text = "Part",
+                    fontWeight = FontWeight.Bold
+                )
+                Row(
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Box(
+                        modifier = Modifier.padding(horizontal = 40.dp)
+                    ) {
+                        Row {
+                            RadioButton(
+                                selected = (part == "Normal"),
+                                onClick = { part = "Normal" }
+                            )
+                            Text(
+                                text = "일반직",
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+                    }
+                    Box(
+                        modifier = Modifier.padding(horizontal = 40.dp)
+                    ) {
+                        Row {
+                            RadioButton(
+                                selected = (part == "Manage"),
+                                onClick = { part = "Manage" }
+                            )
+                            Text(
+                                text = "관리직",
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+                    }
+                }
+            }
+
+            Row {
+
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color(0x59000000)),
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(
+                        text = "ID 찾기",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Button(
+                    onClick = { },
+                    colors = ButtonDefaults.buttonColors(Color(0x59000000)),
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(
+                        text = "PW 찾기",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Text(
+                text = "HeadWear - Intelligence",
+                modifier = Modifier.padding(top = 20.dp),
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}

c83bc22d9df978b955e174b7b277aa3ec9a10613
J_Coder <seok3765@naver.com>
2024-05-02 00:49:10 +0900

    [HW] create: 아두이노 코딩 시작

---
+++ Hardware/headware-Intelligence/headware-Intelligence.ino
@@ -0,0 +1,12 @@
+
+#define SW420 2 // 충격 센서 핀
+#define PIEZO 4 // 피에조 소자
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}

f245fed980e66b2558d8994a529e8062d41020f5
J_Coder <seok3765@naver.com>
2024-05-02 01:01:14 +0900

    [HW] feature: PIN SETUP

--- Hardware/headware-Intelligence/headware-Intelligence.ino
+++ Hardware/headware-Intelligence/headware-Intelligence.ino
@@ -1,6 +1,13 @@
 
-#define SW420 2 // 충격 센서 핀
-#define PIEZO 4 // 피에조 소자
+#define SHOCK 2   // 충격 센서 핀
+#define PIEZO 4   // 피에조 소자
+#define LED 15    // LED
+#define PHOTO 0   // 조도센서
+#define GPS_1 1   // GPS
+#define GPS_2 3   // GPS
+#define GYRO_1 12 // 자이로스코프 센서
+#define GYRO_2 13 // 자이로스코프 센서
+
 void setup()
 {
   // put your setup code here, to run once:

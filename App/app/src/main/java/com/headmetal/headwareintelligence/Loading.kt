266810d6292081813bef86d36cfd6582ea3631f3
right5625 <right5625@naver.com>
2024-05-12 19:01:25 +0900

    [APP] feat: 로딩 구현

--- App/app/src/main/java/com/headmetal/headwareintelligence/Loading.kt
+++ App/app/src/main/java/com/headmetal/headwareintelligence/Loading.kt
@@ -4,28 +4,38 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
-fun Loading(navController: NavController, modifier: Modifier = Modifier) {
-    LaunchedEffect(key1 = true) {
+fun Loading(navController: NavController) {
+    LaunchedEffect(true) {
+        LoadingState.show()
         delay(3000)
+        LoadingState.hide()
         navController.navigate("loginScreen")
     }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFFF9C94C)
     ) {
+        LoadingScreen()
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
@@ -39,5 +49,35 @@ fun Loading(navController: NavController, modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold
             )
         }
+    }
+}
+
+@Composable
+fun LoadingScreen() {
+    val isLoading = LoadingState.isLoading.collectAsState().value
+
+    if (isLoading) {
+        Dialog(
+            onDismissRequest = { LoadingState.hide() },
+            properties = DialogProperties(
+                dismissOnClickOutside = false,
+                dismissOnBackPress = false
+            )
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+object LoadingState {
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    fun show() {
+        _isLoading.value = true
+    }
+
+    fun hide() {
+        _isLoading.value = false
     }
 }

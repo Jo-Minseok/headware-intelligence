de6a410987bd63378fcf168cd125ddea9ed0765f
right5625 <right5625@naver.com>
2024-05-14 22:52:37 +0900

    [APP] feat: 공통 기능 추가

--- App/app/src/main/java/com/headmetal/headwareintelligence/Common.kt
+++ App/app/src/main/java/com/headmetal/headwareintelligence/Common.kt
@@ -1,17 +1,18 @@
 package com.headmetal.headwareintelligence
 
-
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.platform.LocalContext
-import android.app.AlertDialog
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+
+// 처리 상황 코드를 나타내는 열거형 클래스
+enum class SituationCode {
+    COMPLETE, PROCESSING, MALFUNCTION, REPORT119 // 처리 완료 : 0, 처리 중 : 1, 오작동 : 2, 119 신고 : 3
+}
 
 object LoadingState {
     private val _isLoading = MutableStateFlow(false)

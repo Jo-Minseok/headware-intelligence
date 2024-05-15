01c1f444283dea8c9949f247eaa08540bc2d424c
채승룡 <tmdfyd986@gmail.com>
2024-05-15 16:36:14 +0900

    [APP] 기타 및 라이센스 수정

--- App/app/src/main/java/com/headmetal/headwareintelligence/License.kt
+++ App/app/src/main/java/com/headmetal/headwareintelligence/License.kt
@@ -100,7 +100,7 @@ fun License(navController: NavController) {
                             AlertDialog(
                                 onDismissRequest = { apacheDialog = false },
                                 title = {
-                                    Text(text = "MIT License")
+                                    Text(text = "Apache License 2.0")
                                 },
                                 text = {
                                     Column {
@@ -163,25 +163,21 @@ fun License(navController: NavController) {
                                 text = {
                                     Column {
                                         Text(text = "The MIT License (MIT)\n" +
-                                                "\n" +
                                                 "Copyright (c) 2024 Head Metal\n" +
-                                                "\n" +
-                                                "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
-                                                "of this software and associated documentation files (the \"Software\"), to deal\n" +
-                                                "in the Software without restriction, including without limitation the rights\n" +
-                                                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
-                                                "copies of the Software, and to permit persons to whom the Software is\n" +
-                                                "furnished to do so, subject to the following conditions:\n" +
-                                                "\n" +
-                                                "The above copyright notice and this permission notice shall be included in\n" +
-                                                "all copies or substantial portions of the Software.\n" +
-                                                "\n" +
-                                                "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
-                                                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
-                                                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
-                                                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
-                                                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
-                                                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n" +
+                                                "Permission is hereby granted, free of charge, to any person obtaining a copy" +
+                                                "of this software and associated documentation files (the Software), to deal" +
+                                                "in the Software without restriction, including without limitation the rights" +
+                                                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" +
+                                                "copies of the Software, and to permit persons to whom the Software is" +
+                                                "furnished to do so, subject to the following conditions:" +
+                                                "The above copyright notice and this permission notice shall be included in" +
+                                                "all copies or substantial portions of the Software." +
+                                                "THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" +
+                                                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," +
+                                                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" +
+                                                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" +
+                                                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," +
+                                                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN" +
                                                 "THE SOFTWARE.")
                                     }
                                 },
@@ -229,13 +225,11 @@ fun License(navController: NavController) {
                                 text = {
                                     Column {
                                         Text(text = " GNU GENERAL PUBLIC LICENSE\n" +
-                                                "                         Version 2, June 1991\n" +
-                                                "\n" +
-                                                "     Copyright (C) 2024 Free Head Metal.\n" +
-                                                "     59 Temple Place - Suite 330, Boston, MA  02111-1307, USA\n" +
-                                                "\n" +
-                                                "     Everyone is permitted to copy and distribute verbatim copies\n" +
-                                                "     of this license document, but changing it is not allowed.")
+                                                "Version 2, June 1991" +
+                                                " Copyright (C) 2024 Free Head Metal.\n" +
+                                                "59 Temple Place - Suite 330, Boston, MA  02111-1307, USA" +
+                                                " Everyone is permitted to copy and distribute verbatim copies" +
+                                                "of this license document, but changing it is not allowed.")
                                     }
                                 },
                                 confirmButton = {

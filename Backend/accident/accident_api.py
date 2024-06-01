e6bda547c945cf36e8f59f1f6e9d4b4eff2147a2
J_Coder <seok3765@naver.com>
2024-06-02 05:09:15 +0900

    [BE] chore: 이미지 전송 받기

--- Backend/accident/accident_api.py
+++ Backend/accident/accident_api.py
@@ -1,44 +1,27 @@
-from fastapi import APIRouter, HTTPException, WebSocket, Depends, status
+import base64
+import io
+from fastapi import APIRouter, WebSocket, Depends, UploadFile, File, Form
+import os
 from pydantic import BaseModel
 from typing import List
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from starlette.websockets import WebSocketDisconnect
 from sqlalchemy.orm import Session
 import datetime
 from db.db_connection import get_db
-from db.models import Accident, Work
-from pyfcm import FCMNotification
-from db.models import UserEmployee
+from db.models import Accident
 
 router = APIRouter(prefix="/accident")
 
 
 # 사고 발생 Json 구조
 class Accident_Json(BaseModel):
-    category: str
+    type: str
     date: List[int] = []
     time: List[int] = []
-    latitude: float
-    longitude: float
-    work_id: str
-    victim_id: str
+    user_id: str
 
-
-class Alert(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=r'./accident/.env', env_file_encoding='utf-8')
-    api_key: str
-
-
-FCM_API_KEY = Alert(_env_file=r'./accident/.env', _env_file_encoding='utf-8')
-
-
-class Work_list(BaseModel):
-    work_list: List[str] = []
 
 # Websocket 접속 매니저
-
-
 class ConnectionManager:
     def __init__(self):
         self.active_connections = {}
@@ -62,38 +45,42 @@ class ConnectionManager:
 
 
 # 사고 발생시 데이터를 받아오고, 이를 DB에 저장하는 방식
-@router.post("/upload", status_code=status.HTTP_200_OK)
+@router.post("/upload")
 def post_accident(accident: Accident_Json, db: Session = Depends(get_db)):
     db_accident = Accident(date=datetime.date(year=accident.date[0], month=accident.date[1], day=accident.date[2]),
                            time=datetime.time(
                                hour=accident.time[0], minute=accident.time[1], second=accident.time[2]),
-                           latitude=accident.latitude,
-                           longitude=accident.longitude,
-                           work_id=accident.work_id,
-                           victim_id=accident.victim_id,
-                           category=accident.category)
+                           latitude=0.000000,
+                           longitude=0.000000,
+                           victim_id=accident.user_id,
+                           category=accident.type)
     db.add(db_accident)
     db.commit()
-    user = db.query(UserEmployee).filter(
-        UserEmployee.id == accident.victim_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="사용자를 찾을 수 없습니다.")
-    # 사고 발생 시 알림 전송
-    push_service = FCMNotification(FCM_API_KEY.api_key)
-    alert = push_service.multiple_devices_data_message(
-        topic_name=accident.work_id,
-        message_title=f"{accident.category} 사고 발생!",
-        message_body=f"피해자: {user.name} ({accident.victim_id})"
-    )
-    return {"status": "success"}
+    return 200
 
 
-@router.get("/work_list", response_model=Work_list, status_code=status.HTTP_200_OK)
-def get_work_list(user_id: str, db: Session = Depends(get_db)):
-    work_rows = db.query(Work).filter(Work.worker_id == user_id)
-    work_ids = [work_row.work_id for work_row in work_rows]
-    return Work_list(work_list=work_ids)
+@router.post("/upload_image")
+async def upload_image(file: UploadFile = File(...)):
+    try:
+        # 이미지를 저장할 디렉토리 경로 설정
+        save_path = "./accident/uploaded_images/"
 
+        # 저장할 디렉토리가 존재하지 않는 경우 생성
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        # 파일 경로 설정
+        file_path = os.path.join(save_path, file.filename)
+
+        # 이미지 파일을 서버에 저장
+        with open(file_path, "wb") as image_file:
+            image_file.write(await file.read())
+
+        # 성공적으로 저장되었다는 응답 반환
+        return {"message": "File uploaded successfully", "filename": file.filename}
+    except Exception as e:
+        # 오류 발생 시 오류 메시지 반환
+        return {"message": str(e)}
 
 manager = ConnectionManager()
 

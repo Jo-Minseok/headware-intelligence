fcf5acf4cd880f9354b50a1ffc1b4b77171f1f5c
J_Coder <seok3765@naver.com>
2024-06-04 02:30:35 +0900

    [BE] chore: FCM Notification 오류 수정

--- Backend/fcm_notification/fcm_function.py
+++ Backend/fcm_notification/fcm_function.py
@@ -11,17 +11,15 @@ firebase_admin.initialize_app(cred)
 
 def fcm_subscribe_all_topic(manager_id: str, alert_token: str, db: Session):
     work_list_rows = db.query(Work_list.work_id).filter(
-    Work_list.manager == manager_id)
-    work_list_ids = [work_row.work_id for work_row in work_list_rows]
-    for work_id in work_list_ids:
+        Work_list.manager == manager_id).all()
+    for work_id in work_list_rows:
         firebase_admin.messaging.subscribe_to_topic(alert_token, work_id)
 
 
 def fcm_unsubscribe_all_topic(manager_id: str, alert_token: str, db: Session):
     work_list_rows = db.query(Work_list.work_id).filter(
-    Work_list.manager == manager_id)
-    work_list_ids = [work_row.work_id for work_row in work_list_rows]
-    for work_id in work_list_ids:
+        Work_list.manager == manager_id).all()
+    for work_id in work_list_rows:
         firebase_admin.messaging.unsubscribe_from_topic(alert_token, work_id)
 
 
@@ -35,8 +33,9 @@ def fcm_unsubscribe_one_topic(alert_token: str, work_id: str):
 
 def fcm_send_messaging(work_id: str, victim_id: str, db: Session):
     victim_name = db.query(UserEmployee.name).filter(
-        UserEmployee.id == victim_id)
-    work_name = db.query(Work_list.name).filter(Work_list.work_id == work_id)
+        UserEmployee.id == victim_id).scalar()
+    work_name = db.query(Work_list.name).filter(
+        Work_list.work_id == work_id).scalar()
     message = firebase_admin.messaging.Message(
         notification=firebase_admin.messaging.Notification(
             title="사고 발생 삐용삐용",

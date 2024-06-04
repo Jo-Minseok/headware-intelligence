26cc1b36beee2ac74fe4a57d2d455e9973996659
J_Coder <seok3765@naver.com>
2024-06-05 00:18:06 +0900

    [HW] chore: PIN 수정

--- Arduino/ESP32-S3-CAM/Module_Test/CDS_TEST/CDS_TEST.ino
+++ Arduino/ESP32-S3-CAM/Module_Test/CDS_TEST/CDS_TEST.ino
@@ -1,11 +1,19 @@
 #define ESP32_S3_CAM
-#define CDS 10
+#define CDS 8
+#define LED 6
 
 void setup(){
 	Serial.begin(115200);
 	pinMode(CDS,INPUT);
+  pinMode(LED,OUTPUT);
 }
 
 void loop(){
 	Serial.println(analogRead(CDS));
+  if(analogRead(CDS)>=1400){
+    digitalWrite(LED,1);
+  }
+  else{
+    digitalWrite(LED,0);
+  }
 }
--- Arduino/ESP32-S3-CAM/module_pins.h
+++ Arduino/ESP32-S3-CAM/module_pins.h
@@ -1,6 +1,6 @@
 #define GPS_1 43
 #define GPS_2 44
-#define LED 3
+#define LED 6
 
 #define SHOCK 10
 #define CDS 8
@@ -8,3 +8,4 @@
 #define PIEZO 12
 #define GYRO_SCL 14
 #define GYRO_SDA 13
+#define RESET 0

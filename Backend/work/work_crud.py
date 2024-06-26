e3960751943121d6aa39dab0d40f59db691658f6
Jinho <right5625@naver.com>
2024-06-26 20:04:30 +0900

    [BE] feat: 작업장 조회, 수정, 삭제, 근로자 투입 기능 구현

--- Backend/work/work_crud.py
+++ Backend/work/work_crud.py
@@ -1,41 +1,81 @@
 from sqlalchemy import exists
 from sqlalchemy.orm import Session
 from db.models import UserEmployee, WorkList, Work
-from typing import List
+from typing import List, Optional
 from db.db_connection import get_db
 from fastapi import Depends
+from pydantic import BaseModel, validator
+from datetime import datetime
+
+class Validators:
+    @staticmethod
+    def not_empty(v):
+        if not v or not v.strip():
+            raise ValueError('빈 값은 허용되지 않습니다.')
+        return v
+
+class WorkInputCreate(BaseModel):
+    name: str
+    company: str
+    startDate: datetime
+    endDate: Optional[datetime] = None
+
+    @validator('name', 'company', 'startDate', pre=True, always=True)
+    def not_empty(cls, v):
+        return Validators.not_empty(v)
 
 class WorkRepository:
     def __init__(self, db: Session):
         self.db = db
     
-    def get_all_employeeId(self) -> List[str]:
-        employeeRows = self.db.query(UserEmployee).all()
-        return [employeeRow.id for employeeRow in employeeRows]
-
-    def get_manager_by_workId(self, managerId: str) -> List[int]:
-        workRows = self.db.query(WorkList).filter(WorkList.managerId == managerId).all()
-        return [workRow.workId for workRow in workRows]
+    def get_work_list(self, managerId: str) -> List[WorkList]:
+        return self.db.query(WorkList).filter(WorkList.managerId == managerId).all()
+    
+    def create_work(self, managerId: str, data: WorkInputCreate):
+        newWorkList = WorkList(name=data.name, company=data.company, startDate=data.startDate, endDate=data.endDate, managerId=managerId)
+        self.db.add(newWorkList)
+        self.db.commit()
+    
+    def update_work(self, workId: int, data: WorkInputCreate):
+        updateWorkList = self.db.query(WorkList).filter(WorkList.workId == workId).first()
+        updateWorkList.name = data.name
+        updateWorkList.company = data.company
+        updateWorkList.startDate = data.startDate
+        updateWorkList.endDate = data.endDate
+    
+    def delete_work(self, workId: int):
+        deleteWorkList = self.db.query(WorkList).filter(WorkList.workId == workId).first()
+        self.db.delete(deleteWorkList)
+        self.db.commit()
     
     def update_employee_work(self, workId : int, employeeId: str):
-        workExists = self.db.query(exists().where(Work.workId == workId).where(Work.workerId == employeeId)).scalar()
-        if not workExists:
-            newWork = Work(workId=workId, workerId=employeeId)
-            self.db.add(newWork)
-            self.db.commit()
-        return workExists
+        userExists = self.db.query(exists().where(UserEmployee.id == employeeId)).scalar()
+        if userExists:
+            workExists = self.db.query(exists().where(Work.workId == workId).where(Work.workerId == employeeId)).scalar()
+            if not workExists:
+                newWork = Work(workId=workId, workerId=employeeId)
+                self.db.add(newWork)
+                self.db.commit()
+            return workExists
+        return not userExists
 
 class WorkService:
     def __init__(self, repository: WorkRepository):
         self.repository = repository
-        
-    def search_employeeId(self) -> List[str]:
-        return self.repository.get_all_employeeId()
-
-    def search_workId(self, managerId: str) -> List[int]:
-        return self.repository.get_manager_by_workId(managerId)
     
-    def assign_work_employee(self, workId: int, employeeId: str):
+    def search_work_list(self, managerId: str) -> List[WorkList]:
+        return self.repository.get_work_list(managerId)
+    
+    def insert_work(self, managerId: str, data: WorkInputCreate):
+        self.repository.create_work(managerId, data)
+    
+    def modify_work(self, workId: int, data: WorkInputCreate):
+        self.repository.update_work(workId, data)
+    
+    def remove_work(self, workId: int):
+        self.repository.delete_work(workId)
+    
+    def assign_employee_work(self, workId: int, employeeId: str):
         return self.repository.update_employee_work(workId, employeeId)
 
 def get_work_service(db: Session = Depends(get_db)) -> WorkService:
--- Backend/work/work_router.py
+++ Backend/work/work_router.py
@@ -1,28 +1,57 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from work.work_crud import get_work_service
-from work.work_crud import WorkService
+from work.work_crud import WorkService, WorkInputCreate
 
 router = APIRouter(prefix='/work')
 
-@router.get('/search/employee', status_code=status.HTTP_200_OK)
-def get_employee_id(service: WorkService = Depends(get_work_service)):
-    searchResult = service.search_employeeId()
-    if not searchResult:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='근로자가 존재하지 않음')
-    return {'employeeId': searchResult}
-
 @router.get('/search/{managerId}', status_code=status.HTTP_200_OK)
-def get_work_id(managerId: str, service: WorkService = Depends(get_work_service)):
-    searchResult = service.search_workId(managerId)
+def get_employee_id(managerId: str,service: WorkService = Depends(get_work_service)):
+    searchResult = service.search_work_list(managerId)
     if not searchResult:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail='관리자에게 할당된 작업이 존재하지 않음')
-    return {'workId': searchResult}
+    workId = []
+    name = []
+    company = []
+    startDate = []
+    endDate = []
+    for work in searchResult:
+        workId.append(work.workId)
+        name.append(work.name)
+        company.append(work.company)
+        startDate.append(work.startDate)
+        endDate.append(work.endDate)
+    return {
+        'workId': workId, 
+        'name': name, 
+        'company': company, 
+        'startDate': startDate, 
+        'endDate': endDate
+        }
 
-@router.put('/assign/{workId}/{employeeId}', status_code=status.HTTP_200_OK)
-def update_work(workId: int, employeeId: str, service: WorkService = Depends(get_work_service)):
-    updateFailed = service.assign_work_employee(workId, employeeId)
+@router.post('/create/{managerId}', status_code=status.HTTP_200_OK)
+def create_work(managerId: str, inputData: WorkInputCreate, service: WorkService = Depends(get_work_service)):
+    try:
+        service.insert_work(managerId, inputData)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=str(e))
+
+@router.post('/update/{workId}', status_code=status.HTTP_200_OK)
+def update_work(workId: int, inputData: WorkInputCreate, service: WorkService = Depends(get_work_service)):
+    try:
+        service.modify_work(workId, inputData)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=str(e))
+
+@router.put('/delete/{workId}', status_code=status.HTTP_200_OK)
+def delete_work(workId: int, service: WorkService = Depends(get_work_service)):
+    service.remove_work(workId)
+
+@router.post('/assign/{workId}/{employeeId}', status_code=status.HTTP_200_OK)
+def update_employee_work(workId: int, employeeId: str, service: WorkService = Depends(get_work_service)):
+    updateFailed = service.assign_employee_work(workId, employeeId)
     if updateFailed:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='이미 할당된 작업')
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='존재하지 않는 근로자거나 이미 할당된 작업임')

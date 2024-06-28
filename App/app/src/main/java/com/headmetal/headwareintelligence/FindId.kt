7f6931a461595bba46f1d3dc3a32fa5a56bafe58
Jinho <right5625@naver.com>
2024-06-28 22:37:54 +0900

    [APP] refactor: FindId.kt, FindPw.kt 리팩토링

--- App/app/src/main/java/com/headmetal/headwareintelligence/FindId.kt
+++ App/app/src/main/java/com/headmetal/headwareintelligence/FindId.kt
@@ -74,7 +74,7 @@ fun FindId(navController: NavController = rememberNavController()) {
             ) {
                 FieldLabel(text = "직무")
                 Row {
-                    DistinguishButton(
+                    CustomRadioButton(
                         modifier = Modifier
                             .weight(1f)
                             .height(50.dp),
@@ -83,7 +83,7 @@ fun FindId(navController: NavController = rememberNavController()) {
                         secondButtonSwitch = isManager,
                     )
                     Spacer(modifier = Modifier.width(20.dp))
-                    DistinguishButton(
+                    CustomRadioButton(
                         modifier = Modifier
                             .weight(1f)
                             .height(50.dp),

08ffa57bab40fb88230cbfa132beba2ba82b4945
Jinho <right5625@naver.com>
2024-08-16 17:25:34 +0900

    [APP] refactor: CompanyInfo.kt 리팩토링

--- App/app/src/main/java/com/headmetal/headwareintelligence/CompanyInfo.kt
+++ App/app/src/main/java/com/headmetal/headwareintelligence/CompanyInfo.kt
@@ -1,14 +1,16 @@
 package com.headmetal.headwareintelligence
 
 import android.util.Log
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -22,67 +24,6 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-@Composable
-fun CompanyInfo(navController: NavController = rememberNavController()) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color(0xFFF9F9F9)
-    ) { CompanyInfoComposable(navController = navController) }
-}
-
-@Composable
-fun CompanyInfoComposable(navController: NavController = rememberNavController()) {
-    Column {
-        BackIcon(onClick = { navController.navigateUp() })
-        ScreenTitleText(text = "참여 건설 업체")
-        CompanyListComposable()
-    }
-}
-
-@Composable
-fun CompanyListComposable() {
-    var companies by remember { mutableStateOf(listOf<String>()) }
-
-    LaunchedEffect(Unit) {
-        RetrofitInstance.apiService.getCompanyList().enqueue(object : Callback<CompanyList> {
-            override fun onResponse(call: Call<CompanyList>, response: Response<CompanyList>) {
-                if (response.isSuccessful) {
-                    response.body()?.let { companies = it.companies }
-                }
-            }
-
-            override fun onFailure(call: Call<CompanyList>, t: Throwable) {
-                Log.e("HEAD METAL", "서버 통신 실패: ${t.message}")
-            }
-        })
-    }
-
-    LazyColumn(
-        modifier = Modifier
-            .padding(horizontal = 30.dp)
-            .padding(top = 10.dp)
-    ) {
-        items(companies) { company ->
-            CompanyCard { Text(text = company, fontSize = 24.sp) }
-        }
-    }
-}
-
-@Composable
-fun CompanyCard(
-    content: @Composable ColumnScope.() -> Unit = {}
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        shape = RoundedCornerShape(8.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(255, 190, 0, 150)),
-        content = content
-    )
-}
-
-
 // 프리뷰
 @Preview(showBackground = true)
 @Composable
@@ -92,30 +33,79 @@ fun CompanyInfoPreview() {
 
 @Preview(showBackground = true)
 @Composable
-fun CompanyInfoComposablePreview() {
-    CompanyInfoComposable()
+fun CompanyCardListPreview() {
+    val companies by remember { mutableStateOf(listOf("부산 건설", "동의 건설")) }
+
+    LazyColumn(
+        modifier = Modifier
+            .padding(horizontal = 30.dp)
+            .padding(top = 10.dp)
+    ) {
+        items(companies) { company ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(255, 190, 0, 150)),
+                content = { Text(text = company, fontSize = 24.sp) }
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun CompanyInfoBackIconPreview() {
-    BackIcon()
-}
+fun CompanyInfo(navController: NavController = rememberNavController()) {
+    IconScreen(
+        imageVector = Icons.Default.ArrowBackIosNew,
+        onClick = { navController.navigateUp() },
+        content = {
+            var companies by remember { mutableStateOf(listOf<String>()) }
 
-@Preview(showBackground = true)
-@Composable
-fun CompanyInfoTitleTextPreview() {
-    ScreenTitleText(text = "참여 견설 업체")
-}
+            LaunchedEffect(Unit) {
+                RetrofitInstance.apiService.getCompanyList()
+                    .enqueue(object : Callback<CompanyList> {
+                        override fun onResponse(
+                            call: Call<CompanyList>,
+                            response: Response<CompanyList>
+                        ) {
+                            if (response.isSuccessful) {
+                                response.body()?.let { companies = it.companies }
+                            }
+                        }
 
-@Preview(showBackground = true)
-@Composable
-fun CompanyListComposablePreview() {
-    CompanyListComposable()
-}
+                        override fun onFailure(call: Call<CompanyList>, t: Throwable) {
+                            Log.e("HEAD METAL", "서버 통신 실패: ${t.message}")
+                        }
+                    })
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun CompanyCardPreview() {
-    CompanyCard { Text(text = "부산건설", fontSize = 24.sp) }
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ScreenTitleText(text = "참여 건설 업체")
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(horizontal = 30.dp)
+                        .padding(top = 10.dp)
+                ) {
+                    items(companies) { company ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            shape = RoundedCornerShape(8.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = Color(
+                                    255,
+                                    190,
+                                    0,
+                                    150
+                                )
+                            ),
+                            content = { Text(text = company, fontSize = 24.sp) }
+                        )
+                    }
+                }
+            }
+        }
+    )
 }

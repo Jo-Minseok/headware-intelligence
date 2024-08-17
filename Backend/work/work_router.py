54ef6837d8fadcc90f5695bbb5072c5e256683ea
J_Coder <seok3765@naver.com>
2024-08-18 03:16:56 +0900

    fix: 작업 할당된 게 없어도 오류는 발생하지 않게 설정

--- Backend/work/work_router.py
+++ Backend/work/work_router.py
@@ -4,12 +4,13 @@ from work.work_crud import WorkService, WorkInputCreate
 
 router = APIRouter(prefix='/work')
 
+
 @router.get('/search/{managerId}', status_code=status.HTTP_200_OK)
 def get_work_list(managerId: str, service: WorkService = Depends(get_work_service)):
     searchResult = service.search_work_list(managerId)
-    if not searchResult:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='관리자에게 할당된 작업이 존재하지 않음')
+    # if not searchResult:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED, detail='관리자에게 할당된 작업이 존재하지 않음')
     workId = []
     name = []
     company = []
@@ -22,12 +23,13 @@ def get_work_list(managerId: str, service: WorkService = Depends(get_work_servic
         startDate.append(work.startDate)
         endDate.append(work.endDate)
     return {
-        'workId': workId, 
-        'name': name, 
-        'company': company, 
-        'startDate': startDate, 
+        'workId': workId,
+        'name': name,
+        'company': company,
+        'startDate': startDate,
         'endDate': endDate
-        }
+    }
+
 
 @router.post('/create/{managerId}', status_code=status.HTTP_200_OK)
 def create_work(managerId: str, inputData: WorkInputCreate, service: WorkService = Depends(get_work_service)):
@@ -36,6 +38,7 @@ def create_work(managerId: str, inputData: WorkInputCreate, service: WorkService
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail=str(e))
+
 
 @router.get('/search/detail/{workId}', status_code=status.HTTP_200_OK)
 def get_work(workId: int, service: WorkService = Depends(get_work_service)):
@@ -46,9 +49,10 @@ def get_work(workId: int, service: WorkService = Depends(get_work_service)):
         workerId.append(detail[0])
         name.append(detail[1])
     return {
-        'workerId': workerId, 
+        'workerId': workerId,
         'name': name
-        }
+    }
+
 
 @router.post('/update/{workId}', status_code=status.HTTP_200_OK)
 def update_work(workId: int, inputData: WorkInputCreate, service: WorkService = Depends(get_work_service)):
@@ -58,17 +62,20 @@ def update_work(workId: int, inputData: WorkInputCreate, service: WorkService = 
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail=str(e))
 
+
 @router.put('/delete/{workId}', status_code=status.HTTP_200_OK)
 def delete_work(workId: int, service: WorkService = Depends(get_work_service)):
     service.remove_work(workId)
+
 
 @router.get('/user/{employeeId}', status_code=status.HTTP_200_OK)
 def get_employee(employeeId: str, service: WorkService = Depends(get_work_service)):
     searchResult = service.search_employee(employeeId)
     return {
-        'name': searchResult.name, 
+        'name': searchResult.name,
         'phoneNo': searchResult.phoneNo
     }
+
 
 @router.put('/assign/{workId}/{employeeId}', status_code=status.HTTP_200_OK)
 def create_employee_work(workId: int, employeeId: str, service: WorkService = Depends(get_work_service)):
